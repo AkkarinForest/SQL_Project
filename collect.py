ec4222a97f590b26e561188fb5ae2fcf1a7191a6
AkkarinForest <akkarinforest@gmail.com>
2018-02-09 11:08:24 +0100

    Deleting unnecessary print statements.

--- collect.py
+++ collect.py
@@ -4,26 +4,20 @@ import sys
 import pymysql.cursors
 
 connection = pymysql.connect(host='127.0.0.1', port=3306, user='me', passwd='', db='tcp')
-
-print("started")
 cur = connection.cursor()
 
-print("started")
 for line in sys.stdin:
-    if line:
-        print(line.split())
+
     linesplit=line.split()
     lineip=linesplit[0].split(',')
-    print(line)
     ip=lineip[0]
+
     timee=linesplit[1].split('.')
     time=timee[0]
-    print(ip, time)
 
     with connection.cursor() as cursor:
 
         sql = "INSERT INTO collect (ip, first, last) VALUES (%s,%s,%s) ON DUPLICATE KEY UPDATE last=%s;"
-        print("inserting")
         cursor.execute(sql, (ip, time, time, time))
         connection.commit()
 
--- collectI2.py
+++ collectI2.py
@@ -7,24 +7,24 @@ connection = pymysql.connect(host='127.0.0.1', port=3306, user='me', passwd='', 
 
 cur = connection.cursor()
 
-print("started")
+
 for line in sys.stdin:
     linesplit=line.split()
+
     if len(linesplit)>=3:
-        print(linesplit[2])
         url=linesplit[2]
         lineip = linesplit[0].split(',')
-        print(line)
+
         ip = lineip[0]
         timee = linesplit[1].split('.')
         time = timee[0]
+
         with connection.cursor() as cursor:
 
             sql = "INSERT INTO urls(url) VALUES(%s) on duplicate key update url = url"
             cursor.execute(sql, (url))
 
             sql2 = "INSERT INTO collect (ip, first, last) VALUES (%s,%s,%s) ON DUPLICATE KEY UPDATE last=%s;"
-            print("inserting")
             cursor.execute(sql2, (ip, time, time, time))
 
             sql3="INSERT INTO pairs(url, ip) VALUES(%s, %s) on duplicate key update url = url, ip = ip"
